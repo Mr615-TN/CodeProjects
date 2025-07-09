7d39fef9df611ecbd4288e49707ca7389ab1a654
Tanish Neema <tneema501@gmail.com>
2025-07-09 13:38:23 -0500

    Update

--- C_CPP/Basic_OperatingSystems/cpu/idt.c
+++ C_CPP/Basic_OperatingSystems/cpu/idt.c
@@ -5,11 +5,11 @@ struct idt_entry idt[256];
 struct idt_reg idt_reg;
 
 void set_idt_gate(int n, uint32_t handler) {
-    idt[n].low_offset = low_16(handler);
+    idt[n].base_low = low_16(handler);
     idt[n].sel = KERNEL_CS;
     idt[n].always0 = 0;
     idt[n].flags = 0x8E; 
-    idt[n].high_offset = high_16(handler);
+    idt[n].base_high = high_16(handler);
 }
 
 void set_idt() {
