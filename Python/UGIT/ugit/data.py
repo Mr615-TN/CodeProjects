88d9230aeb3d212b30fe523833c5ca72aeb11406
Tanish Neema <tneema501@gmail.com>
2025-09-01 17:09:01 -0500

    Update

--- Python/UGIT/ugit/data.py
+++ Python/UGIT/ugit/data.py
@@ -8,9 +8,14 @@ def init():
     os.makedirs (f'{GIT_DIR}/objects', exist_ok=True)
 
 def set_HEAD(oid):
+    with open(f'{GIT_DIR}/HEAD', 'w') as f:
+        f.write(oid)
+
+def get_HEAD():
     if os.path.isfile(f'{GIT_DIR}/HEAD'):
-        with open(f'{GIT_DIR}/HEAD', 'w') as f:
-            f.write(oid)
+        with open(f'{GIT_DIR}/HEAD', 'r') as f:
+            return f.read().strip()
+    return None
 
 def hash_object(data, type_='blob'):
     obj = type_.encode() + b'\x00' + data
